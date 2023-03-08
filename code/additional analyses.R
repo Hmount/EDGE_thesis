26c48e64c58e91d145019766099a938a8b4324ed
Hmount <haileymount11@gmail.com>
2023-03-08 16:53:21 -0700

    Answering reviewer 1 comments. Completeing formal analysis on if differences in lifespan effect popualtion responses. They differ, but overall no effect on the main finding. Also begining to create simulated data to do a  comparison between my findings and a null model.

--- code/additional analyses.R
+++ code/additional analyses.R
@@ -1,6 +1,16 @@
 #### additional analyses ####
-# These anlayses were done, but did not make it into the final paper:
+# These analyses were done, but did not make it into the final paper:
 # do pop.responses differ by basic life history traits? (no)
+
+library(tidyverse)
+library(ggpubr)
+
+NEWallsite <- read.csv("data/allsite_new.csv") #data
+NEWallsite <- NEWallsite %>% #ensure levels are correct
+  mutate(grassland_type = fct_relevel(grassland_type,
+                                      "Desert", "Southern Shortgrass", "Northern Shortgrass", 
+                                      "Northern Mixed", "Southern Mixed", "Tallgrass"))
+
 
 #### do pop.responses differ by basic life history traits? ####
 # response to drought may differ slightly by lifespan, but non-sig.
@@ -8,9 +18,32 @@
 # response to nieghbors in ambient does differ on the grasslands by lifespan, life form, and photo-type 
 
 #response to drought
-anova(lm(intrinsicdiff~lifespan*grassland_type,data=NEWallsite))#slightly sig in anova, not in lm
-anova(lm(intrinsicdiff~grass.forb*grassland_type,data=NEWallsite))#nah
-anova(lm(intrinsicdiff~Photosynthesis*grassland_type,data=NEWallsite[-c(125,136),]))#nah
+NEWallsite <- NEWallsite %>% mutate(lifespan.binary = ifelse(lifespan %in% "Short-lived","0", "1")) #make binary
+NEWallsite$lifespan.binary <- as.factor(NEWallsite$lifespan.binary)
+var.test(NEWallsite$intrinsicdiff~NEWallsite$lifespan)
+t.test(NEWallsite$intrinsicdiff~NEWallsite$lifespan, var.equal = T) #annuals have more positive responses to drought than perennials
+ggplot(NEWallsite, aes(fill=lifespan, y=intrinsicdiff))+
+  geom_boxplot() +
+  theme_minimal()
+
+var.test(NEWallsite$chrdiff~NEWallsite$lifespan)
+t.test(NEWallsite$chrdiff~NEWallsite$lifespan, var.equal = T) #no diff
+
+var.test(NEWallsite$condiff~NEWallsite$lifespan)
+t.test(NEWallsite$condiff~NEWallsite$lifespan, var.equal = T) #no diff
+
+#compare response models
+main <- lm(chrdiff~intrinsicdiff, weights=weight2, NEWallsite[-106,]) #new main finding
+anova(main) #trade-off
+sqrt(summary(intchr)$adj.r.squared)#calculate r
+#run second model w/ grassland
+main.lifespan <- lm(chrdiff~intrinsicdiff*lifespan.binary, weights=weight2, NEWallsite[-106,]) #old finding 
+anova(main.lifespan) 
+#compare w/ anova
+anova(main,main.lifespan)
+
+
+
 
 #response to neighbors drought
 anova(lm(chrdiff~lifespan*grassland_type,data=NEWallsite))#nah
@@ -30,6 +63,7 @@ ggplot(NEWallsite, aes(y=condiff, x=Photosynthesis))+
 anova(lm(condiff~Photosynthesis*grassland_type,data=NEWallsite[-c(125,136),]))#grassland
 
 
+
 #### test if difference in traits (dissimilarity from CWM) improved fitness (it did not) ####
 #find difference between CWM and focal traits
 test <- trtdata %>% mutate(traitdiff = SLA.x-SLA.y)
@@ -39,3 +73,20 @@ ggplot(test, aes(y=chrdiff, x=traitdiff))+#, color=grassland_type))+
   geom_smooth(method="lm")
 ### pop responses as a function of trait difference
 summary(lm(chrdiff~traitdiff, test))
+
+#### ensure that ratio correlations are not mathematically inevitable)
+#make simulated data
+rinvA <- runif(100, -1,4)
+rinvD <- runif(100, -1,4)
+rintA <- runif(100, -1,4)
+rintD <- runif(100, -1,4)
+
+#find differences
+intdif <- rinvA - 
+invdif
+condif
+chrdif
+
+#compare correlations amoung random variables
+cor.test()
+
